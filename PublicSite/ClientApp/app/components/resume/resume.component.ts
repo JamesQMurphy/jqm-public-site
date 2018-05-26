549ad974dfafe9549131f193696e479dab3f9f52
James Murphy <jamesqmurphy@yahoo.com>
2018-05-25 20:51:35 -0400

    Releases/0.0.0 (#2)
    Initial setup
    * Copied MileageLog and renamed to PublicSite
    
    * Renamed PublicSite namespace to JamesQMurphy.PublicSite namespace
    
    * Missed a namespace reference in the razor file
    
    * Removed an explicit file reference to an Angular component
    
    * Added resume component
    
    * Create buildspec.yml
    
    * Fixed format of YAML file
    
    * Removed “version” text
    
    * Copied example YAML
    
    * Changed npm ci to npm install
    
    * Moved npm install to pre_build phase; changed directory
    
    * Build release configuration instead of debug
    
    * Added printenv command
    
    * Added command to list current branch
    
    * Ugh, still getting used to YAML syntax
    
    * Tried older git command
    
    https://stackoverflow.com/a/1418022/1001100
    
    * Trying git status instead
    
    * Tried different command to get the branch name
    
    https://stackoverflow.com/a/2707110/1001100
    
    * View remote branches only
    
    * Removed call to git branch
    
    AWS Code Pipeline assigns a completely different variable to CODEBUILD_SOURCE_VERSION.  Something like this:
    arn:aws:s3:::codepipeline-us-east-1-466944808800/JQM-Publicsite-Pipel/MyApp/9ILBcvc.zip

---
+++ PublicSite/ClientApp/app/components/resume/resume.component.ts
@@ -0,0 +1,8 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+    selector: 'resume-component',
+    templateUrl: './resume.component.html'
+})
+export class ResumeComponent {
+}
