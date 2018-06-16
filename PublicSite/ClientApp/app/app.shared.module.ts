d84cf2a8cdf3d462f788c903c2cb0e58f9aad30e
James Murphy <JamesQMurphy@yahoo.com>
2018-06-16 17:57:43 -0400

    Replaced VehicleList with BlogArticleList

--- PublicSite/ClientApp/app/app.shared.module.ts
+++ PublicSite/ClientApp/app/app.shared.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { ResumeComponent } from './components/resume/resume.component';
+import { BlogArticleListComponent } from './components/blogarticlelist/blogarticlelist.component';
 import { VehicleListComponent } from './components/vehiclelist/vehiclelist.component';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { VehicleListComponent } from './components/vehiclelist/vehiclelist.compo
         NavMenuComponent,
         HomeComponent,
         ResumeComponent,
+        BlogArticleListComponent,
         VehicleListComponent
     ],
     imports: [
@@ -26,6 +28,7 @@ import { VehicleListComponent } from './components/vehiclelist/vehiclelist.compo
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'resume', component: ResumeComponent },
+            { path: 'blog', component: BlogArticleListComponent },
             { path: 'vehicles', component: VehicleListComponent },
             { path: '**', redirectTo: 'home' }
         ])
---
+++ PublicSite/Controllers/BlogArticleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JamesQMurphy.PublicSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PublicSite.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class BlogArticleController : Controller
+    {
+        // TODO: temporarily serve up some objects
+        private BlogArticle[] _blogArticles = new BlogArticle[]
+        {
+            new BlogArticle()
+            {
+                Title = "Hello, Blog!",
+                PublishDate = new DateTime(2018, 6, 10, 8, 30, 0),
+                Slug = "hello-blog",
+                Content = "Hello, and welcome to my blog!"
+            },
+
+            new BlogArticle()
+            {
+                Title = "Welcome back!",
+                PublishDate = new DateTime(2018, 6, 11, 13, 15, 0),
+                Slug = "welcome-back",
+                Content = "Welcome back!  This is my second entry."
+            },
+
+        };
+
+        [HttpGet()]
+        public IEnumerable<BlogArticle> GetAll()
+        {
+            return _blogArticles;
+        }
+    }
+}
