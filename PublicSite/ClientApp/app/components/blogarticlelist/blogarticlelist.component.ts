8c1d9945c5d2663cfcecd0c2c389151c048906d2
James Murphy <jamesqmurphy@yahoo.com>
2018-06-16 22:35:10 -0400

    Replaced VehicleList with BlogArticleList & home page changes (#6)

---
+++ PublicSite/ClientApp/app/components/blogarticlelist/blogarticlelist.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'blogarticle-list',
+    templateUrl: './blogarticlelist.component.html'
+})
+export class BlogArticleListComponent {
+    public blogarticles: blogarticle[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/BlogArticle').subscribe(result => {
+            this.blogarticles = result.json() as blogarticle[];
+        }, error => console.error(error));
+    }
+}
+
+interface blogarticle {
+    title: string;
+    publishDate: Date;
+    slug: string;
+    content: string;
+}
