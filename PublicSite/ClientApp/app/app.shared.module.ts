08485c9195b7f4737f74c63d4cfa51bd1eed77bf
James Murphy <JamesQMurphy@yahoo.com>
2018-07-05 01:42:28 -0400

    separated Routes as object, put page title in Routes data

--- PublicSite/ClientApp/app/app.shared.module.ts
+++ PublicSite/ClientApp/app/app.shared.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router, Routes } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { BlogArticleListComponent } from './components/blogarticlelist/blogarticlelist.component';
+
+const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent, data: { title: 'One DevOps, please!' } },
+    { path: 'resume', component: ResumeComponent, data: { title: 'Resume - One DevOps, please!' }  },
+    { path: 'blog', component: BlogArticleListComponent, data: { title: 'Blog - One DevOps, please!' }  },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
     declarations: [
@@ -22,13 +30,7 @@ import { BlogArticleListComponent } from './components/blogarticlelist/blogartic
         CommonModule,
         HttpModule,
         FormsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'resume', component: ResumeComponent },
-            { path: 'blog', component: BlogArticleListComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
+        RouterModule.forRoot(appRoutes)
     ]
 })
 export class AppModuleShared {
